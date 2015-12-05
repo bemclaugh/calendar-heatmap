7f04e3c18a3a9a0a38a223826bfec1f2675c749d
Brian McLaughlin <bemclaugh@gmail.com>
2015-12-05 11:40:52 -1000

    Updated to standard js style

--- app/main.jsx
+++ app/main.jsx
@@ -1,18 +1,20 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var _ = require('underscore');
-var Calendar = require('./components/Calendar.jsx');
+var React = require('react')
+var ReactDOM = require('react-dom')
+var _ = require('underscore')
+require('./main.scss')
+var Calendar = require('./components/Calendar.jsx')
 
-var testData = function() {
-	var m = _.range(12);
-	var d = _.range(1, 32);
-	var data = {};
-	for(var i in m) {
-		for(var j in d) {
-			var key = m[i] + '_' + d[j];
-			data[key] = Math.floor(Math.random() * (100 - 1)) + 1;
-		}
-	}
-	return(data);
-};
-ReactDOM.render(<Calendar fillData={testData()}/>, document.getElementById('calendar'));
+var testData = function () {
+  var m = _.range(12)
+  var d = _.range(32)
+  const y = '2015'
+  var data = {}
+  for (var i in m) {
+    for (var j in d) {
+      var key = y + '-' + ('0' + (m[i] + 1)).slice(-2) + '-' + ('0' + (d[j] + 1)).slice(-2)
+      data[key] = Math.floor(Math.random() * (100 - 1)) + 1
+    }
+  }
+  return (data)
+}
+ReactDOM.render(<Calendar fillData={testData()} />, document.getElementById('calendar'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-var Webpack = require('webpack');
-var path = require('path');
-var nodeModulesPath = path.resolve(__dirname, 'node_modules');
-var buildPath = path.resolve(__dirname, 'public', 'build');
-var mainPath = path.resolve(__dirname, 'app', 'main.jsx');
-var sassPath = path.resolve(__dirname, 'app', 'main.scss');
-var colorMaps = path.resolve(nodeModulesPath, 'colormap', 'colorScales.json');
+var Webpack = require('webpack')
+var path = require('path')
+var nodeModulesPath = path.resolve(__dirname, 'node_modules')
+var buildPath = path.resolve(__dirname, 'public', 'build')
+var mainPath = path.resolve(__dirname, 'app', 'main.jsx')
+var sassPath = path.resolve(__dirname, 'app', 'main.scss')
+var colorMaps = path.resolve(nodeModulesPath, 'colormap', 'colorScales.json')
 
 var config = {
   devtool: 'eval',
-  //devtool: 'eval-source-map',
+  // devtool: 'eval-source-map',
   entry: ['webpack/hot/dev-server', 'webpack-dev-server/client?http://localhost:8080', mainPath],
   output: {
     path: buildPath,
@@ -18,14 +18,14 @@ var config = {
   module: {
     loaders: [
       {test: colorMaps, loader: 'json'},
-      {test: /\.jsx$/, loader: 'babel-loader', exclude: [nodeModulesPath],query: {presets:['react']}},
-      {test: /\.scss$/, loaders: ["style", "css", "sass"]}
+      {test: /\.jsx$/, loader: 'babel-loader', exclude: [nodeModulesPath], query: {presets: ['react']}},
+      {test: /\.scss$/, loaders: ['style', 'css', 'sass']}
     ]
   },
   plugins: [new Webpack.HotModuleReplacementPlugin()],
   sassLoader: {
     includePaths: [sassPath]
-  },
-};
+  }
+}
 
-module.exports = config;
+module.exports = config
